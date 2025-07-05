b845d2f4440198e0290ce7ecdf2868005847f6b8
meator <meator.dev@gmail.com>
2025-07-05 15:13:20 +0200

    Add wrap file

---
+++ generate_wrap_file.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+import string
+import hashlib
+
+_buffer_size = 8192
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input_release_archive",
+        help=" ".join(
+            (
+                "Input .tar.gz file containing prebuilt libraries. Its",
+                "sha256sum will be computed.",
+            )
+        ),
+    )
+    parser.add_argument(
+        "output_file",
+        help="File into which should the processed version of AdbWinApi.wrap.in be put",
+    )
+    parser.add_argument(
+        "project_version",
+        nargs="?",
+        help=" ".join(
+            (
+                "Version of AdbWinApi project. If unset, use VERSION.txt",
+                "in the same directory this script is located in.",
+            )
+        ),
+    )
+    args = parser.parse_args()
+
+    # Argument validation and processing.
+
+    script_dir = pathlib.Path(__file__).parent
+    dest_dir = pathlib.Path(args.destination_directory)
+
+    if args.project_version:
+        project_version = args.project_version
+    else:
+        with open(script_dir / "VERSION.txt", "r") as file:
+            project_version = file.read().strip()
+
+    sha256sum = hashlib.sha256(usedforsecurity=False)
+    with open(args.input_release_archive, "rb") as file:
+        while True:
+            chunk = file.read(_buffer_size)
+            if not chunk:
+                break
+            sha256sum.update(chunk)
+
+    with open(script_dir / "AdbWinApi.wrap.in", "r") as file:
+        wrap_file_contents = string.Template(file.read()).substitute(
+            version=project_version,
+            sha256sum=sha256sum.hexdigest(),
+        )
+
+    # Write the result into destination directory.
+
+    with open(args.project_version, "w") as file:
+        file.write(project_version)
